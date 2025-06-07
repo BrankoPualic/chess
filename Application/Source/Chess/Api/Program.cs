f56acc67c8462a1915b2de3fea523728bd4d4337
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-06-07 14:27:25 +0200

    feat - initial signalR + typescript configuration

---
+++ Application/Source/Chess/Api/Program.cs
@@ -0,0 +1,31 @@
+using Api.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Services.AddSignalR(options =>
+{
+	options.EnableDetailedErrors = true;
+});
+
+var app = builder.Build();
+app.Use(async (context, next) =>
+{
+	try
+	{
+		await next();
+	}
+	catch (Exception ex)
+	{
+		var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+		logger.LogError(ex, "Unhandled exception");
+		throw;
+	}
+});
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/hub");
+
+app.Run();
